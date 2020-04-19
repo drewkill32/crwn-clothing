09692eedb5ea33bdb31769809fdb5eb558415343
Drew Killion <adk@killion.me>
2020-04-19 14:17:20 -0400

    update menu items to navigate with router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
 
 
 import HomePage from './pages/homepage/homepage.component'
+
+
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route path='/' component= { HomePage } />
+      </Switch>
+
     </div>
   );
 }
