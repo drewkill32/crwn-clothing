d6a4d22cf876212b5c0d3724c0ddbcf90e493519
Drew Killion <adk@killion.me>
2020-04-19 20:45:14 -0400

    adding signin

--- src/App.js
+++ src/App.js
@@ -6,13 +6,17 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/signin-and-sign-up/signin-and-sign-up.component'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route  exact path='/' component= { HomePage } />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUp} />
       </Switch>
 
     </div>
