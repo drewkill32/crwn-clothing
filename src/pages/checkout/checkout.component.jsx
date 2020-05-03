62cb8ec9ec467d66ca2db6069a7daa4a2ab3df58
Drew Killion <adk@killion.me>
2020-05-03 16:03:34 -0400

    add item and remove item to component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -12,7 +12,6 @@ import {
 } from "../../redux/cart/cart.selectors";
 
 import "./checkout.styles.scss";
-import CartItem from "../../components/cart-item/cart-item.component";
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,22 +1,42 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
-    const existingCartItem = cartItems.find(
-        cartItem => {
-            return cartItem.id === cartItemToAdd.id;
-        }
+  const existingCartItem = cartItems.find((cartItem) => {
+    return cartItem.id === cartItemToAdd.id;
+  });
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToAdd.id
+        ? {
+            ...cartItem,
+            quantity: cartItem.quantity + 1,
+          }
+        : cartItem
     );
+  }
 
-    if (existingCartItem) {
-        return cartItems.map(cartItem =>
-            cartItem.id === cartItemToAdd.id ? {
-                ...cartItem,
-                quantity: cartItem.quantity + 1
-            } :
-            cartItem);
+  return [
+    ...cartItems,
+    {
+      ...cartItemToAdd,
+      quantity: 1,
+    },
+  ];
+};
 
-    }
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find((cartItem) => {
+    return cartItem.id === cartItemToRemove.id;
+  });
 
-    return [...cartItems, {
-        ...cartItemToAdd,
-        quantity: 1
-    }]
-}
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? {
+          ...cartItem,
+          quantity: cartItem.quantity - 1,
+        }
+      : cartItem
+  );
+};
