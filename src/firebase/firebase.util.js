6dddee9878d40d96dc3496d490c24c6d9e3ee37c
Drew Killion <adk@killion.me>
2020-05-07 21:13:44 -0400

    added shop data to firebase

--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -1,57 +1,65 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
+// Your web app's Firebase configuration
+const config = {
+  apiKey: "AIzaSyAw0y_y0J1y3BmGMhdXHYT-ekbL6ilig3s",
+  authDomain: "crwn-db-166dd.firebaseapp.com",
+  databaseURL: "https://crwn-db-166dd.firebaseio.com",
+  projectId: "crwn-db-166dd",
+  storageBucket: "crwn-db-166dd.appspot.com",
+  messagingSenderId: "10563902820",
+  appId: "1:10563902820:web:51d8c98f41ee28bfc16e71",
+  measurementId: "G-LJ2WBRZ58N",
+};
 
-  
-  // Your web app's Firebase configuration
-  const config = {
-    apiKey: "AIzaSyAw0y_y0J1y3BmGMhdXHYT-ekbL6ilig3s",
-    authDomain: "crwn-db-166dd.firebaseapp.com",
-    databaseURL: "https://crwn-db-166dd.firebaseio.com",
-    projectId: "crwn-db-166dd",
-    storageBucket: "crwn-db-166dd.appspot.com",
-    messagingSenderId: "10563902820",
-    appId: "1:10563902820:web:51d8c98f41ee28bfc16e71",
-    measurementId: "G-LJ2WBRZ58N"
-  };
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
 
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShop = await userRef.get();
 
-  export const createUserProfileDocument = async (userAuth, additionalData) => {
-
-    if (!userAuth) return;
-
-    const userRef = firestore.doc(`users/${userAuth.uid}`);
-    const snapShop = await userRef.get();
-    
-    if (!snapShop.exists) {
-        const { displayName, email } = userAuth;
-        const createdAt = new Date();
-        try {
-            await userRef.set({
-                displayName,
-                email,
-                createdAt,
-                 ...additionalData
-                });
-        } catch (error) {
-            console.log('error creating user', error.message);
-        }
+  if (!snapShop.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
     }
-    return userRef;
   }
+  return userRef;
+};
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore();
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+  const batch = firestore.batch();
+  objectsToAdd.forEach((obj) => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
 
+  await batch.commit();
+};
 
-  const provider = new firebase.auth.GoogleAuthProvider();
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
 
-  provider.setCustomParameters({prompt: 'select_account'});
+const provider = new firebase.auth.GoogleAuthProvider();
 
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+provider.setCustomParameters({ prompt: "select_account" });
 
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
-  export default firebase;
+export default firebase;
